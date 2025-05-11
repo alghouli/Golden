be432f9acc872cadcc33c0487b0a6f50812aea7e
alghouli <46010114+alghouli@users.noreply.github.com>
2025-05-11 15:38:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,90 @@
+(function ($) {
+  'use strict';
+
+  /* ========================================================================= */
+  /*	Page Preloader
+  /* ========================================================================= */
+
+  // window.load = function () {
+  // 	document.getElementById('preloader').style.display = 'none';
+  // }
+
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
+
+
+  //Hero Slider
+  $('.hero-slider').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+    dots: false,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
+
+
+  /* ========================================================================= */
+  /*	Header Scroll Background Change
+  /* ========================================================================= */
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 200) {
+      //console.log('a');
+      $('.navigation').addClass('sticky-header');
+    } else {
+      //console.log('a');
+      $('.navigation').removeClass('sticky-header');
+    }
+  });
+
+  /* ========================================================================= */
+  /*	On scroll fade/bounce effect
+  /* ========================================================================= */
+  var scroll = new SmoothScroll('a[href*="#"]');
+
+  // -----------------------------
+  //  Count Up
+  // -----------------------------
+  function counter() {
+    if ($('.counter').length !== 0) {
+      var oTop = $('.counter').offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 1000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          }
+        });
+      });
+    }
+  }
+  // -----------------------------
+  //  On Scroll
+  // -----------------------------
+  $(window).scroll(function () {
+    counter();
+  });
+
+})(jQuery);
